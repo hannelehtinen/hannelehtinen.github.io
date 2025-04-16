1982c2cb44288bd15b82a30080df14b31127526a
hhh <n3674@student.jamk.fi>
2025-04-16 17:11:14 +0300

    fixed navi + responsivity

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navig");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+navSlide();
+
+
+
 /* -----------------------------------------
   Have focus outline only for keyboard users 
  ---------------------------------------- */
