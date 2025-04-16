8a8fd76d62c46d82f53af2952bb7ae89a0de3bf8
hannelehtinen <hannelehtinen@outlook.com>
2025-04-16 13:21:11 +0300

    testinavi

--- index.js
+++ index.js
@@ -152,13 +152,3 @@ document.addEventListener("keydown", (event) => {
     }
   }
 });
-
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".navig");
-
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-  });
-};
-navSlide();
